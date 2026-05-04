2e89472c6455ec65cb5ae299ebe2fc0380b105ee
zhengliu <ykcome@gmail.com>
2026-05-04 23:12:12 +0800

    Remove root Python scripts

--- add_swift_file.py
+++
@@ -1,9 +0,0 @@
-import sys
-import re
-import uuid
-
-proj_path = 'TexasPoker.xcodeproj/project.pbxproj'
-with open(proj_path, 'r') as f:
-    content = f.read()
-
-# Instead of manually parsing, just put CircularImageCropper code into PlayerProfileView.swift
